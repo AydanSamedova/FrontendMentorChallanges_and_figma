b05d1e7eddd00a88b2f11ee0a02768168cafb0b3
AydanSamedova <aydan2903@gmail.com>
2021-03-31 19:34:35 +0400

    landing

--- 06.landing page/easybank-landing-page-master/app/js/app.js
+++ 06.landing page/easybank-landing-page-master/app/js/app.js
@@ -4,12 +4,9 @@ const navbar = document.querySelector('.header_navbar')
 
 
 
-
+// =============sidebar menu hamburger==========//
 btnHamburger.addEventListener('click', function() {
-
     btnHamburger.classList.toggle("open");
-    overlay.classList.toggle('overlay-visible')
-
-    navbar.classList.toggle('navbar-visible')
-
+    overlay.classList.toggle('overlay-visible');
+    navbar.classList.toggle('navbar-visible');
 });
